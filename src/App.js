9d59c4d536d8ee3ac2766f2d2fb9c4e2c26ea7eb
Jonathan Diego Rdz2 <jon_eldiablo@hotmail.com>
2024-01-06 16:10:46 -0600

    rename init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import components from "dbl-components/src/js/components";
+import info from "dbl-components/tmp/components-info.json";
 
 function App() {
+  const docs = Object.entries(components).map(([key, C]) => [key, C]);
+  console.log(components);
+  console.log("------");
+  console.log(info);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>Hola mundo!!!</>
   );
 }
 
